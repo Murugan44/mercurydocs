83a442188ab2bb027deae6772edb44c346e2d29e
Murugan44 <kbm4497@gmail.com>
2021-05-18 09:15:07 +0530

    first commit

--- lwc-services.config.js
+++ lwc-services.config.js
@@ -1,7 +1,8 @@
 // Find the full example of all available configuration options at
 // https://github.com/muenzpraeger/create-lwc-app/blob/main/packages/lwc-services/example/lwc-services.config.js
 module.exports = {
-    resources: [{ from: 'src/resources/', to: 'dist/resources/' }
+    buildDir: './dist',
+    resources: [{ from: 'src/resources/', to: 'docs/resources/' }
         
         
         
